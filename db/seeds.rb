d9fae14785ed8e91a3822fd7605a19e6a54e517b
Rebeca Casa <rebi_britto@hotmail.com>
2019-02-20 09:42:52 +0100

    added gem and seeded country name

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Country.destroy_all
+User.destroy_all
+admin = User.new(email: 'admin@test.com', password: 'admin@test.com')
+admin.save!
+
+CS.countries.to_h.each do |key, country|
+  country = Country.new(name: country, user_id: admin.id)
+  country.save!
+end
+
+puts " done!"
