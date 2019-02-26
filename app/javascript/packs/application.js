4563c1ee5dfbb989c50436a021821b4180449272
Rebeca Casa <rebi_britto@hotmail.com>
2019-02-26 16:00:33 +0100

    map is now showing, added title to navbar, fix zoom on map

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,9 +8,3 @@
 // layout file, like app/views/layouts/application.html.erb
 
 
-
-import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
-
-import { initMapbox } from '../plugins/init_mapbox';
-
-initMapbox();
