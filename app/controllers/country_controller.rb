ec1041cdbf2efb7f58c0c4a9f59f0f9a3718b528
Rebeca Casa <rebi_britto@hotmail.com>
2019-02-27 17:01:39 +0100

    added latitude and longitude to countries

--- app/controllers/country_controller.rb
+++ app/controllers/country_controller.rb
@@ -1,18 +1,18 @@
 class CountryController < ApplicationController
   def index
     @countries = Country.all
-
-    # @markers = @countries.map do |country|
-    #   {
-    #     lng: country.longitude,
-    #     lat: country.latitude
-    #   }
-    # end
   end
 
   def show
     @country = Country.find(params[:id])
   end
 
-end
+  def update
+    @country = Country.find(params[:id])
+    @country.update(country_params)
+  end
 
+  def country_params
+    params.require(:country).permit(:visited, :planned)
+  end
+end
